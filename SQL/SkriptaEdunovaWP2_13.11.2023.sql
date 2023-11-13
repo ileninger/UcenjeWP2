da2dfc56e25f670e9cdbf2d0c228046b1f6efb65
mijatovicvalentin <mijatovic.valentin123@gmail.com>
2023-11-13 18:56:44 +0100

    Predavanje 13.11.2023

---
+++ SQL/SkriptaEdunovaWP2_13.11.2023.sql
@@ -0,0 +1,63 @@
+use master;
+go
+
+drop database if exists edunovawp2;
+go
+
+create database edunovawp2;
+go
+
+use edunovawp2;
+
+create table smjerovi(
+sifra int not null primary key identity(1,1),
+naziv varchar (60) not null,
+brojsati int,
+cijena decimal (18,2),
+upisnina decimal (18,2),
+verificiran bit,
+);
+
+create table grupe (
+sifra int not null primary key identity (1,1),
+naziv varchar (50) not null,
+predavac int,
+smjer int not null,
+maksimalnopolaznika int,
+datumpocetka date,
+);
+
+create table predavaci(
+sifra int not null primary key identity(1,1),
+ime varchar (50) not null,
+prezime varchar (50) not null,
+oib char (11),
+email varchar(100) not null,
+iban varchar(50),
+strucnasprema int
+);
+
+create table polaznici (
+sifra int not null primary key identity(1,1),
+ime varchar (50) not null,
+prezime varchar (50) not null,
+oib char (11),
+email varchar(100) not null,
+brojugovora varchar (20)
+);
+
+create table clanovi(
+grupa int not null,
+polaznik int not null
+);
+
+create table strucnasprema (
+sifra int not null primary key identity (1,1),
+vrstaspreme varchar(20) 
+);
+
+alter table grupe add foreign key (smjer) references  smjerovi (sifra);
+alter table grupe add foreign key (predavac) references predavaci (sifra);
+alter table clanovi add foreign key (grupa) references grupe (sifra);
+alter table clanovi add foreign key (polaznik) references polaznici (sifra);
+alter table predavaci add foreign key (strucnasprema) references strucnasprema (sifra);
