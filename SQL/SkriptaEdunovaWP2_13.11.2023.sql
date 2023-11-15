fb12dbd4ea9bd9e19cfbf50f90ab5ec8cd1eafa4
mijatovicvalentin <mijatovic.valentin123@gmail.com>
2023-11-15 17:40:52 +0100

    proba

--- SQL/SkriptaEdunovaWP2_13.11.2023.sql
+++ SQL/SkriptaEdunovaWP2_13.11.2023.sql
@@ -1,49 +1,50 @@
+﻿
 use master;
 go
-
 drop database if exists edunovawp2;
 go
 
 create database edunovawp2;
 go
-
 use edunovawp2;
 
 create table smjerovi(
 sifra int not null primary key identity(1,1),
-naziv varchar (60) not null,
+naziv varchar(60) not null,
 brojsati int,
-cijena decimal (18,2),
-upisnina decimal (18,2),
-verificiran bit,
+cijena decimal(18,2),
+upisnina decimal(18,2),
+verificiran bit
 );
 
-create table grupe (
-sifra int not null primary key identity (1,1),
-naziv varchar (50) not null,
+
+create table grupe(
+sifra int not null primary key identity(1,1),
+naziv varchar(50) not null,
 predavac int,
 smjer int not null,
 maksimalnopolaznika int,
-datumpocetka date,
+datumpocetka datetime
 );
+
 
 create table predavaci(
 sifra int not null primary key identity(1,1),
-ime varchar (50) not null,
-prezime varchar (50) not null,
-oib char (11),
+ime varchar(50) not null,
+prezime varchar(50) not null,
+oib char(11),
 email varchar(100) not null,
-iban varchar(50),
-strucnasprema int
+iban varchar(50)
 );
 
-create table polaznici (
+
+create table polaznici(
 sifra int not null primary key identity(1,1),
-ime varchar (50) not null,
-prezime varchar (50) not null,
-oib char (11),
+ime varchar(50) not null,
+prezime varchar(50) not null,
+oib char(11),
 email varchar(100) not null,
-brojugovora varchar (20)
+brojugovora varchar(20)
 );
 
 create table clanovi(
@@ -51,14 +52,10 @@ grupa int not null,
 polaznik int not null
 );
 
-create table strucnasprema (
-sifra int not null primary key identity (1,1),
-vrstaspreme varchar(20) 
-);
+-- vanjski ključevi
+alter table grupe add foreign key (smjer) references smjerovi(sifra);
+alter table grupe add foreign key (predavac) references predavaci(sifra);
+alter table clanovi add foreign key (grupa) references grupe(sifra);
+alter table clanovi add foreign key (polaznik) references polaznici(sifra);
 
---dodavanje kljuceva
-alter table grupe add foreign key (smjer) references  smjerovi (sifra);
-alter table grupe add foreign key (predavac) references predavaci (sifra);
-alter table clanovi add foreign key (grupa) references grupe (sifra);
-alter table clanovi add foreign key (polaznik) references polaznici (sifra);
-alter table predavaci add foreign key (strucnasprema) references strucnasprema (sifra);
+
