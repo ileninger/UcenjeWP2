11a5b32cd8a678b2d4b9be525d7f7612a2491fb2
Ivan Leninger <ileninger@live.com>
2023-11-23 20:34:50 +0100

    Dovrsen zadatak

--- SQL/Vodoinstalater.sql
+++ SQL/Vodoinstalater.sql
@@ -1,21 +1,52 @@
 use edunovawp2;
 go
 
-drop database if exists Vodoinstaler;
+drop database if exists Vodoinstalateri;
 go
 
-create database Vodoinstalater;
+create database Vodoinstalateri;
 go
 
-Create table StrucneSpreme(
-StrucnaSprema_ID int primary key identity (1,1) not null,
-StrucnaSprema varchar (50)
+use Vodoinstalateri;
+
+create table Strucna(
+Sifra int primary key identity (1,1) not null,
+Naziv varchar (50)
 );
 
 create table Vodoinstalateri (
-Vodoinstalater_ID int primary key identity (1,1) not null,
-Ime varchar (50),
-Prezime varchar (50),
-OiB char (11),
-StrucnaSprema int references StrucneSpreme(StrucnaSprema_ID),
+Sifra int primary key identity (1,1) not null,
+Ime varchar (50) not null,
+Prezime varchar (50) not null,
+OiB char (11) not null,
+StrucnaSprema int references Strucna(Sifra) not null,
+Kontakt varchar (100)
+);
+
+create table Segrti (
+Sifra int primary key identity (1,1) not null,
+Ime varchar (50) not null,
+Prezime varchar (50) not null,
+OiB char (11) not null,
+StrucnaSprema int references Strucna(Sifra) not null,
+Kontakt varchar (100)
+);
+
+create table Popravci (
+Popravak_ID int primary key identity (1,1) not null,
+Vodoinstalater int references Vodoinstalateri (Sifra) not null,
+Segrt int references Segrti (Sifra) not null,
+Datum date not null,
+Cijena decimal (18,2) not null
+);
+
+create table Kvarovi (
+Kvar_ID int primary key identity (1,1) not null,
+Naziv varchar (50),
+Opis varchar (200),
+);
+
+create table Kvarovi_Popraci (
+Kvar_ID int references Kvarovi (Kvar_ID) not null,
+Popravci int references Popravci (Popravak_ID) not null
 );
