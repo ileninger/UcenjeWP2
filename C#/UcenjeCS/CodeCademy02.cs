a70ab6da430e830068ed43d25ce8ffc3deaca5bf
Ivan Leninger <ileninger@live.com>
2023-12-16 21:22:17 +0100

    Ucenje CodeCademy_02/2
    Jupiter years

--- C#/UcenjeCS/CodeCademy02.cs
+++ C#/UcenjeCS/CodeCademy02.cs
@@ -22,8 +22,32 @@ namespace UcenjeCS
             //Console.WriteLine(weight);
             //Console.WriteLine(spayed);
 
-            Console.Write("Enter your favorite number!: ");
-            int faveNumber = Convert.ToInt32(Console.ReadLine());
+            //Console.Write("Enter your favorite number!: ");
+            //int faveNumber = Convert.ToInt32(Console.ReadLine());
+
+            //int pizzaShops = 4332;
+            //int totalEmployees = 86928;
+            //decimal revenue = 390819.28m;
+
+            //Console.WriteLine(pizzaShops);
+            //Console.WriteLine(totalEmployees);
+            //Console.WriteLine(revenue);
+
+            int userAge=int.Parse(Console.ReadLine());
+            double jupiterYears = 11.86;
+
+            double jupiterAge = userAge / jupiterYears;
+
+            double journeyToJupiter = 6.142466;
+
+            double newEarthAge = userAge + journeyToJupiter;
+
+            double newJupiterAge = newEarthAge / jupiterYears;
+
+            Console.WriteLine(jupiterAge);
+            Console.WriteLine(newEarthAge);
+            Console.WriteLine(newJupiterAge);
+
 
         }
     }
