18afb3119f53c4835a350a05b8a4fc9a6b6c04cb
Ivan Leninger <ileninger@live.com>
2023-12-17 12:40:00 +0100

    Ucenje CodeCademy_02/4

--- C#/UcenjeCS/CodeCademy02.cs
+++ C#/UcenjeCS/CodeCademy02.cs
@@ -49,18 +49,95 @@ namespace UcenjeCS
             //Console.WriteLine(newJupiterAge);
 
 
-            int numberOne = 12932;
-            int numberTwo = -2828472;
+            //int numberOne = 12932;
+            //int numberTwo = -2828472;
 
-            // Use built-in methods and save to variable 
-            double numberOneSqrt = Math.Floor(Math.Sqrt(numberOne));
-            double numberTwoSqrt = Math.Floor(Math.Sqrt(Math.Abs(numberTwo)));
+            //// Use built-in methods and save to variable 
+            //double numberOneSqrt = Math.Floor(Math.Sqrt(numberOne));
+            //double numberTwoSqrt = Math.Floor(Math.Sqrt(Math.Abs(numberTwo)));
 
-            // Use built-in methods and save to variable 
+            //// Use built-in methods and save to variable 
 
 
-            // Print the lowest number
-            Console.WriteLine(Math.Min(numberOneSqrt,numberTwoSqrt));
+            //// Print the lowest number
+            //Console.WriteLine(Math.Min(numberOneSqrt,numberTwoSqrt));
+
+            //double numberOne = 6.5;
+            //double numberTwo = 4;
+
+            //// Raise numberOne to the numberTwo power
+            //Console.WriteLine(Math.Pow(numberOne,numberTwo));
+
+            //// Round numberOne up
+            //Console.WriteLine(Math.Round(numberOne));
+
+            //// Find the larger number between numberOne and numberTwo
+            //Console.WriteLine();
+
+            //string firstSentence = "It is a truth universally acknowledged, that a single man in possession of a good fortune, must be in want of a wife.";
+
+
+            // Second string variable
+            //string firstSpeech = "My dear Mr. Bennet," \"said his lady to him one day, \" "\have you heard that Netherfield Park is let at last ?\";
+
+
+            //// Print variable and newline
+
+            //Console.WriteLine(firstSentence);
+            //Console.WriteLine("\n");
+            //Console.WriteLine(firstSpeech);
+
+            //string beginning = "Ovo je početak";
+            //string middle = "Ovo je sredina";
+            //string end = "Ovo je kraj";
+
+
+            //// Concatenate the string and the variables
+            //string story = beginning + middle + end;
+
+            //// Print the story to the console 
+
+            //Console.WriteLine(story);
+
+            //string beginning = "Once upon a time,";
+            //string middle = "The kid climbed a tree";
+            //string end = "Everyone lived happily ever after.";
+
+            //string story = $"{beginning} it was a beautiful day. {middle} and the birds flew through the sky. {end}";
+            //Console.WriteLine(story);
+
+            // Create password
+            string password = "a92301j2add";
+
+            //// Get password length
+            //int passwordLength = password.Length;
+
+            //Console.WriteLine(passwordLength);
+
+
+            //// Check if password uses symbol
+            //int passwordCheck = password.IndexOf("!");
+
+
+            // Print results
+            //.WriteLine($"The user password is {password}. Its length is {passwordLength} and it receives a {passwordCheck} check.");
+
+            string startStrand = "ATGCGATGAGCTTAC";
+
+            // find location of "tga"
+            int tga = startStrand.IndexOf("TGA");
+
+            // start point and stop point variables
+            int startPoint = 0;
+            int length = tga + 3;
+
+            // define final strand
+            string dna = startStrand.Substring(startPoint, length);
+            Console.WriteLine(dna);
+
+            // DNA mutation search
+            Console.WriteLine(dna[3]);
+
 
 
         }
