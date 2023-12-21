2d8956566e09b9e9dc118d70588a8ac598109ac5
Ivan Leninger <ileninger@live.com>
2023-12-21 23:58:15 +0100

    Provjera cijelog broja

---
+++ C#/UcenjeCS/CijeliBroj.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UcenjeCS
+{
+    internal class CijeliBroj
+    {
+        public static void Izvedi ()
+        {
+            Console.Write("Unesite cijeli broj: ");
+            var CijeliBroj = int.Parse(Console.ReadLine());
+
+            if (CijeliBroj %1 == 0 )
+            {
+                Console.WriteLine("Unjeli ste cijeli broj");
+
+            } else
+            {
+                Console.WriteLine("Niste unjeli cijeli broj");
+            }
+
+
+        }
+    }
+}
