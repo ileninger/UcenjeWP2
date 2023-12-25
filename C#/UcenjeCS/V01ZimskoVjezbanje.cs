6087e36acacc28655a442581f6652e80f209c2ad
Ivan Leninger <ileninger@live.com>
2023-12-25 20:24:07 +0100

    UcenjeCS - Zimski zadaCI
    Kreirati klasu V01ZimskoVjezbanje te slijedno jedno za drugim riješiti sljedeće zadatke
    1. Napisati program koji ispisuje sve brojeve od 1 do 100
    2. Napisati program koji ispisuje sve brojeve od 100 do 1
    3. Napisati program koji ispisuje sve brojeve od 1 do 100 koji su cjelobrojno djeljivi s 7
    4. Napisati program koji unosi brojeve sve dok ne unese broj veći od 100, a zatim ispisuje koliko je bilo pokušaja unosa

---
+++ C#/UcenjeCS/V01ZimskoVjezbanje.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UcenjeCS
+{
+    internal class V01ZimskoVjezbanje
+    {
+        public static void Izvedi()
+        {
+            for (int i = 1; i <= 100; i++)
+            {
+                Console.WriteLine(i);
+            }
+
+            Console.WriteLine("****************");
+
+            Console.WriteLine("EVO DEKREMENTA");
+
+            for (int i = 100; i >= 1; i--)
+            {
+                Console.WriteLine(i);
+            }
+
+            Console.WriteLine("BROJEVI DJELJIVI S 7");
+
+            for (int i = 1; i <= 100; i++)
+            {
+                if (i % 7 == 0) Console.WriteLine(i);
+            }
+            int BrojUnosa = 0;
+            for (; ; )
+            {
+                Console.Write("Unesite broj veci od 100: ");
+                int UneseniBroj = int.Parse(Console.ReadLine());
+                if (UneseniBroj <= 100)
+                {
+                    BrojUnosa++;
+                } else
+                {
+                    Console.WriteLine($"Pokušali ste unjeti broj manji od 100 {BrojUnosa} puta.");
+                    break;
+                }
+
+            }
+        }
+    }
+}
