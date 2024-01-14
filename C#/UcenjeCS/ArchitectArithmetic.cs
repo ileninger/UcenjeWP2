ddf54c5cab80acc2518405aebd3275b3fc10ccd6
Ivan Leninger <ileninger@live.com>
2024-01-14 21:54:41 +0100

    CodeCademy ArhitectAritmetic
    Osnovni zadatak

---
+++ C#/UcenjeCS/ArchitectArithmetic.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UcenjeCS
+{
+    internal class ArchitectArithmetic
+    {
+        static double Rect(double length, double width)
+        {
+            return length * width;
+        }
+
+        static double Circle(double radius)
+        {
+            return (Math.PI * (radius * radius));
+        }
+
+        static double Triangle(double bottom, double height)
+        {
+            return (0.5*bottom*height);
+        }
+        public static void Izvedi() {
+            
+            double TotalArea = (Rect(1500,2500)) + (Triangle(750,500))+ ((Circle(375)/2));
+            int PricePerArea = 180;
+            double TotalPrice = TotalArea*PricePerArea;
+            Console.WriteLine($"My plan costs:{Math.Round(TotalPrice,2)} pasos");
+
+        }
+    }
+}
---
+++ C#/UcenjeCS/CodeCademyMethode.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UcenjeCS
+{
+    internal class CodeCademyMethode
+    {
+        public static void Izvedi()
+        {
+            string msg = "Yabba dabba doo!";
+
+            Math.Min(3, 5);
+
+            Console.WriteLine(msg);
+
+            Console.WriteLine(msg.Substring(0, 1)); 
+        }
+    }
+}
