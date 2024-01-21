0e8db163e4bd0ed378b5fd4a3cfd8b24c99c5e51
Ivan Leninger <ileninger@live.com>
2024-01-21 21:23:54 +0100

    Napravljeno do rekurzije

--- C#/UcenjeCS/LjubavniKalkulator.cs
+++ C#/UcenjeCS/LjubavniKalkulator.cs
@@ -11,69 +11,13 @@ namespace UcenjeCS
         public static void Izvedi()
         {
             string PrvoIme = UnesiIme("Unesite prvo ime: ");
-            //string DrugoIme = UnesiIme("Unesite drugo ime: ");
-            Console.WriteLine($"Unjeli ste kao prvo ime {PrvoIme}");
-            BrojPonavljanjaSlovaUnutarImena(PrvoIme);
+            string DrugoIme = UnesiIme("Unesite drugo ime: ");
+
+            Console.WriteLine(Kalkulator(PocetniNiz (PrvoIme,DrugoIme)));
+
+
         }
-
-        private static void BrojPonavljanjaSlovaUnutarImena(string a)
-        {
-            int[] Slova = new int[a.Length]; // Pravimo niz u onoj dužini koliko elemenata ima uneseni tekst
-            int Index = 0;
-            int Rezultat;
-            foreach (char c in a)
-            {
-                Rezultat = 0;
-                foreach (char cc in a)
-                {
-                    if (c == cc)
-                    {
-                        Rezultat++;
-                    }
-                }
-                Slova[Index++] = Rezultat;
-            }
-            char[] JedinstvenoSlovo = new char[a.Length]; // Pravimo novi niz samo od unesenih slova u duljini unosa
-            bool Postoji;
-            Index = 0;
-
-            foreach (char c in a)
-            {
-                Postoji = false;
-
-                foreach (char cc in JedinstvenoSlovo)
-                {
-                    if (c == cc)
-                    {
-                        Postoji = true;
-                        break;
-                    }
-                }
-                if (!Postoji)
-                {
-                    JedinstvenoSlovo[Index++] = c;
-                }
-            }
-            //Console.WriteLine(string.Join(",", JedinstvenoSlovo));
-            Console.WriteLine(string.Join(",", Slova));
-            //foreach (char c in JedinstvenoSlovo)
-            //{
-            //    Console.Write(c + " ");
-            //    Index = 0;
-
-            //    foreach (char cc in a)
-            //    {
-            //        if (c == cc)
-            //        {
-            //            Console.WriteLine(Slova[Index]);
-
-            //            break;
-            //        }
-            //        Index++;
-            //    }
-            //}
-        }
-
+      
         private static string UnesiIme(string v)
         {
             for (; ; )
@@ -90,5 +34,77 @@ namespace UcenjeCS
                 }
             }
         }
+
+
+        private static int[] PocetniNiz (string PrvoIme, string DrugoIme ) 
+        {
+            string SpojenaImena = PrvoIme + DrugoIme;
+            int[] NizZaAnalizu = new int[SpojenaImena.Length];
+            int i = 0;
+            
+
+            // Sada cemo prebrojati koliko se puta koje slovo pojavljuje u nizu koji se sastoji od prvog i  drugog imena
+
+            foreach (char c in SpojenaImena)
+            {
+                int Brojac = 0;
+
+                foreach (char cc in SpojenaImena)
+                {
+                    if (c == cc) // Ako se isti znak vec pojavljuje
+                    {
+                        Brojac++;
+                    }
+                }
+                NizZaAnalizu[i++] = Brojac;
+            }
+
+            Console.WriteLine(SpojenaImena + "\n" + string.Join(" ",NizZaAnalizu));
+
+            return NizZaAnalizu;
+        }
+
+        private static string Kalkulator (int[] PrimjeniNiz)
+        {
+            ///Definiramo "pozicije s kojih nam ide brojac unutar niza Min i Max
+            //int Min = 0;
+            //int Max = PrimjeniNiz.Length;
+            string Novi = null;
+            int Brojac = 0;
+            int Rez;
+            int DuzinaPocetnogNiza = PrimjeniNiz.Length;
+            int[] Rezultat = new int[DuzinaPocetnogNiza / 2 + (DuzinaPocetnogNiza % 2)]; ;
+
+            //while (Min < Max)
+            //{
+                //int LijeviDio = PrimjeniNiz[Min];
+                //int DesniDio = PrimjeniNiz[Max];
+
+                //for (int i = Min; i < Max; i++)
+                //{
+                //    Rezultat[i] = LijeviDio + (DesniDio-i-1);
+                //}
+                //Min++;
+                //Max--;
+
+
+                for (int i = 0; i < DuzinaPocetnogNiza / 2; i++)
+                {
+                    Rezultat[i] = PrimjeniNiz[i] + PrimjeniNiz[DuzinaPocetnogNiza - i - 1];
+                }
+
+                if (DuzinaPocetnogNiza % 2 == 1)
+                {
+                    Rezultat[DuzinaPocetnogNiza / 2] = PrimjeniNiz[DuzinaPocetnogNiza / 2];
+                }
+
+
+
+            //}
+
+            return string.Join(" ",Rezultat);
+        }
+
+
     }
 }
