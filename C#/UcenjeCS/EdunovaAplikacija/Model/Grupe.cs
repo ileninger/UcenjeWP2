cb1c95475c14d862a35e9517962a6f3ab0882cec
Ivan Leninger <ileninger@live.com>
2024-02-01 18:26:46 +0100

    Dodan predavac u grupe

--- C#/UcenjeCS/EdunovaAplikacija/Model/Grupe.cs
+++ C#/UcenjeCS/EdunovaAplikacija/Model/Grupe.cs
@@ -9,10 +9,17 @@ namespace UcenjeCS.EdunovaAplikacija.Model
     internal class Grupe:Entitet
     {
         public string Naziv { get; set; }
-        public DateTime DatumPocetka { get; set; }
 
+        public Predavac Predavac{ get; set; }
+       
         public Smjer Smjer { get; set; }
 
+        public int MaksimalanBrojPolaznika{ get; set; }
+
+        public DateTime DatumPocetka { get; set; }
+
         public List <Polaznik> Polaznici { get; set; }
+
+        p
     }
 }
