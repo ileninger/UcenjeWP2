c88ffd94661d7324225a79cfc97974bf8179d27a
Ivan Leninger <ileninger@live.com>
2024-02-04 18:48:52 +0100

    Napravljen glavni izbornik i uređena početna poruka

--- C#/UcenjeCS/ZavrsniRad/Program.cs
+++ C#/UcenjeCS/ZavrsniRad/Program.cs
@@ -8,18 +8,55 @@ namespace UcenjeCS.ZavrsniRad
 {
     internal class Program
     {
+        public ObradaRadnika ObradaRadnici { get; }
         public Program()
         {
+            ObradaRadnici = new ObradaRadnika();
             PozdravnaPoruka();
+            PrikaziGlavniIzbornik();
         
             
         }
 
+        private void PrikaziGlavniIzbornik()
+        {
+            Console.WriteLine("Glavni izbornik");
+            Console.WriteLine("1. Radnici");
+            Console.WriteLine("2. Obračini");
+            Console.WriteLine("3. Plače");
+            Console.WriteLine("4. Izlaz iz programa");
+            OdabirGralvniIzbornik();
+        }
+
+        private void OdabirGralvniIzbornik()
+        {
+            switch(Pomocno.UcitajRasponBrojeva("Odaberite stavku izbornika: ", "Odabir mora biti 1-4. ", 1, 4))
+            {
+                case 1:
+                    ObradaRadnici.PrikaziIzbornikObradaRadnika();
+                    OdabirGralvniIzbornik();
+                    break;
+                case 2:
+                    //ObradaObracun;
+                    OdabirGralvniIzbornik();
+                    break;
+                case 3:
+                    //ObradaPlaca;
+                    OdabirGralvniIzbornik();
+                    break;  
+                case 4:
+                    Console.WriteLine("Hvala na korištenju, doviđenja");
+                    break;
+
+            }
+        }
+
         private void PozdravnaPoruka()
         {
-            Console.WriteLine("****************************");
-            Console.WriteLine("OBRACUN PLAČA BY LENY_V1");
-            Console.WriteLine("****************************");
+            Console.WriteLine("***************************************");
+            Console.WriteLine("********OBRACUN PLAČA BY LENY_V1*******");
+            Console.WriteLine("***************************************");
+
         }
     }
 }
